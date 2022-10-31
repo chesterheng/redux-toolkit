e9fb190c756b40f73da703ef387e1aa497a1262d
Chester Heng <chester.heng@gmail.com>
2022-11-01 01:40:32 +0800

    17.Modal Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Navbar } from "./components/Navbar";
 import { CartContainer } from "./components/CartContainer";
+import { Modal } from "./components/Modal";
 
 function App() {
   return (
     <main>
       <Navbar />
+      <Modal />
       <CartContainer />
     </main>
   );
